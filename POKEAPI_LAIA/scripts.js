a9faef36ea39a7f85715ae9e9f8e74e5413aa32f
laiaxarau <laiaxarau@gmail.com>
2023-07-27 22:57:49 +0200

    pokeapi

---
+++ POKEAPI_LAIA/scripts.js
@@ -0,0 +1,35 @@
+async function pokeapi(endpoint) {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  return data.results;
+}
+
+const pokeData = await pokeapi("https://pokeapi.co/api/v2/pokemon?limit=50");
+
+async function loadTemplate(templateSearch) {
+  const response = await fetch("templates.html");
+  const templates = document.createElement("template");
+  templates.innerHTML = await response.text();
+  console.log(templates);
+  const template = templates.content.getElementById(templateSearch);
+  return template.content;
+}
+
+async function carregarPokemon(pokemonData) {
+  const smallTemplate = await loadTemplate("pokemonCardSmall");
+  const nodeSmallTemplate = smallTemplate.cloneNode(true);
+
+  const pokemonNameArray = pokemonData.url.split("/");
+  const pokedexNumber = pokemonNameArray[pokemonNameArray.length - 2];
+  console.log(pokedexNumber);
+
+  const pokemonImageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokedexNumber}.svg`;
+
+  nodeSmallTemplate.getElementById("pokemonImage").src = pokemonImageURL;
+  nodeSmallTemplate.querySelector("#pokemonName").textContent =
+    pokemonData.name;
+
+  document.getElementById("pokemonList").appendChild(nodeSmallTemplate);
+}
+
+pokeData.map((pokemon) => carregarPokemon(pokemon));
